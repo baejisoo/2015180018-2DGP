95b164212a546d2a81b025becf96261bad9f7bf8
baejisoo(KPU_PC_40) <fanatic0717@naver.com>
2018-09-18 17:49:50 +0900

    코드 기본 구조 완성
    계속 반복되는 기본 구조의 뼈대를 만들었다

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -7,5 +7,16 @@ character = load_image('character.png')
 
 # 여기를 채우세요.
 
+def make_rectangle():
+    pass
+
+def make_circle():
+    pass
+
+while True:
+    make_rectangle()
+    make_circle()
+
+
 close_canvas()
 
