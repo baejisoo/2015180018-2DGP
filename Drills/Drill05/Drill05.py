2a7e85b629f070c6c0099e3bef8991b5a6999487
baejisoo(KPU_PC_40) <fanatic0717@naver.com>
2018-09-18 18:34:55 +0900

    코드 기본 구조 완성
    좌표의 list화, 코드의 뼈대 완성했다

--- Drills/Drill05/Drill05.py
+++ Drills/Drill05/Drill05.py
@@ -7,7 +7,23 @@ character = load_image('character.png')
 
 # 여기를 채우세요.
 
+routine =[(203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
 
+def make_dir():
+    pass
+def move_by_dir():
+    pass
+def make_animation():
+    pass
+
+def make_routine():
+    make_dir()
+    move_by_dir()
+    make_animation()
+    pass
+
+while True:
+    make_routine()
 
 close_canvas()
 
