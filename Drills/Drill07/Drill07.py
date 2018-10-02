bd601f369726d6074cfbf3a0a9951889eea70239
baejisoo <fanatic0717@naver.com>
2018-10-02 18:51:03 +0900

    move character함수
    캐릭터가 랜덤 좌표에 따라 이동하도록 완성했다

--- Drills/Drill07/Drill07.py
+++ Drills/Drill07/Drill07.py
@@ -14,6 +14,13 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def move_character(p1, p2):
+    for i in range(0, 100 + 1, 2):
+        t = i / 100
+        x = (1 - t) * p1[0] + t * p2[0]
+        y = (1 - t) * p1[1] + t * p2[1]
+        return x, y
+
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
@@ -22,18 +29,21 @@ running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 
-size = 4
+size = 10
 n = 1
 points = [(random.randint(- (KPU_WIDTH // 2), KPU_WIDTH // 2), random.randint(-(KPU_HEIGHT // 2), KPU_HEIGHT // 2)) for i in range(size)]
+
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    x, y = move_character(points[n-1], points[n])
+    n = (n + 1) % size
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x , y)
     update_canvas()
     frame = (frame + 1) % 8
 
-    delay(0.02)
+    delay(0.1)
     handle_events()
 
 close_canvas()
