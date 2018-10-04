91a4d4fcdada40ec47a88616d6c48ca1b2c64cab
baejisoo <fanatic0717@naver.com>
2018-10-04 18:15:17 +0900

    곡선 함수
    곡선을 그리기 위한 함수를 완성했다.

--- Drills/Drill08/Drill08-2.py
+++ Drills/Drill08/Drill08-2.py
@@ -14,13 +14,19 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def draw_curve_3_points(p1, p2, p3):
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0] + (2 * t ** 2 - t) * p3[0]
+        y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1] + (2 * t ** 2 - t) * p3[1]
+    return x, y
+
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-
 
 def move_character(p1, p2):
     frame = 0
