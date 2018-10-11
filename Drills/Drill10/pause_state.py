dc40363bdec200b738545d03b0d854c1354a268e
baejisoo(Desktop) <fanatic0717@naver.com>
2018-10-11 19:44:21 +0900

    Drill10-1 완성
    Drill10-1을 완성했다

--- Drills/Drill10/pause_state.py
+++ Drills/Drill10/pause_state.py
@@ -25,7 +25,7 @@ def handle_events():
             if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
                 game_framework.quit()
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
-                game_framework.push_state(main_state)
+                game_framework.pop_state()
 
 def draw():
     clear_canvas()
